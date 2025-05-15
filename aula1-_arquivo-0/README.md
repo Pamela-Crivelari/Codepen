1ea2b88cce0ea7e94e2ba83c3f0464214853548b
Pamela Crivelari <pamelacrivelari@gmail.com>
2025-05-15 15:30:26 -0300

    Delete aula1-_arquivo-0 directory

--- aula1-_arquivo-0/README.md
+++
@@ -1,6 +0,0 @@
-# AULA1 _Arquivo 0
-
-A Pen created on CodePen.
-
-Original URL: [https://codepen.io/Pamela-Crivelari/pen/yyywJQj](https://codepen.io/Pamela-Crivelari/pen/yyywJQj).
-
--- aula1-_arquivo-0/src/script.js
+++
@@ -1,3 +0,0 @@
-valoraneldopoder = prompt("digite a quantidade de anéis do poder")
-umaneldopoder = 959529.86
-alert("O valor convertido em reais é R$" + valoraneldopoder * umaneldopoder)
